a720997729bf003fb5c01bcb4d9bd75687dc08b4
Shivvardhan <shivvardhan52@gmail.com>
2025-03-16 23:43:55 +0530

    new

---
+++ src/test/java/com/pdea/SgrscollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgrscollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
