8abc2d5d46e3fc25dd8df918d1986509fea5ac62
Shivvardhan <shivvardhan52@gmail.com>
2025-03-17 01:39:22 +0530

    new controller

---
+++ src/main/java/com/pdea/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.pdea.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+	@GetMapping("admin")
+	public String get() {
+		return "add";
+	}
+}
