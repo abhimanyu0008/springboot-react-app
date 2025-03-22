34bda952453ddec6d5e4ee8e3de55c1dae5291a6
Shivvardhan <shivvardhan52@gmail.com>
2025-03-22 20:11:03 +0530

    new

--- src/services/admin/studentService.jsx
+++ src/services/admin/studentService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8088/students';
+const API_URL = 'http://javadev-env.eba-4ynrvfyj.us-east-2.elasticbeanstalk.com/students';
 
 // Fetch all students
 const fetchStudents = async () => {
